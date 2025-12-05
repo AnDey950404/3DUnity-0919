225276bbb29d04a86b662769f42748528dfe6dea
AnDey950404 <yixan0303@gmail.com>
2025-12-05 11:09:39 +0800

    子彈射擊敵人扣血

--- Assets/scrieps/npcgo.cs
+++ Assets/scrieps/npcgo.cs
@@ -1,35 +1,48 @@
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
 
 public class npcgo : MonoBehaviour
 {
     private NavMeshAgent 導航;
     private Animator 動畫器;
     public Transform 目標;
-    public float 距離 = 0f;
+    public float 距離 = 0;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TextMeshPro 血量文字;
+    public int 血量 = 100;
+    public Transform 血條;
+
     void Start()
     {
         導航 = GetComponent<NavMeshAgent>();
         動畫器 = GetComponent<Animator>();
-        
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if(目標!=null)
+        if (目標 != null)
         {
             導航.SetDestination(目標.position);
             距離 = Vector3.Distance(目標.position, this.transform.position);
-            if(距離<=2f)
+            if (距離 <= 3.1f) { 動畫器.SetBool("isWalk", false); }
+            else { 動畫器.SetBool("isWalk", true); }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet")
+        {
+            Destroy(other.gameObject);
+            血量--;
+            血量文字.text = 血量.ToString();
+            float 血量比例 = (float)血量 / 100f;
+            print(血量比例);
+            血條.localScale = new Vector3(血量比例, 1, 1);
+
+            if(血量 <= 0)
             {
-                動畫器.SetBool("npcgo", false);
-            }
-            else
-            {
-                動畫器.SetBool("npcgo", true);
+                Destroy(this.gameObject);
             }
         }
     }
