f5780c8ec9e8cccc81cb1816c8e1178f0a641db2
AnDey950404 <yixan0303@gmail.com>
2025-12-12 11:02:10 +0800

    將施攻擊、受傷

--- Assets/scrieps/npcgo.cs
+++ Assets/scrieps/npcgo.cs
@@ -12,20 +12,42 @@ public class npcgo : MonoBehaviour
     public TextMeshPro 血量文字;
     public int 血量 = 100;
     public Transform 血條;
+    int 原始血量;
+    bool 開始攻擊 = false;
+    float 攻擊間距 = 2f;
+    float 下次可攻擊時間;
 
     void Start()
     {
         導航 = GetComponent<NavMeshAgent>();
         動畫器 = GetComponent<Animator>();
+        原始血量 = 血量;
+        血量文字.text = 血量.ToString();
     }
     void Update()
     {
+
+
         if (目標 != null)
         {
             導航.SetDestination(目標.position);
             距離 = Vector3.Distance(目標.position, this.transform.position);
-            if (距離 <= 3.1f) { 動畫器.SetBool("isWalk", false); }
-            else { 動畫器.SetBool("isWalk", true); }
+            if (距離 <= 3.1f) 
+            {
+                動畫器.SetBool("IsAttack", true);
+                動畫器.SetBool("npcgo", false); 
+            }
+            else 
+            { 
+                動畫器.SetBool("npcgo", true); 
+
+            }
+
+            if(開始攻擊)
+            {
+                攻擊時間 = Time.time;
+                if(攻擊時間 = )
+            }
         }
     }
 
@@ -36,13 +58,22 @@ public class npcgo : MonoBehaviour
             Destroy(other.gameObject);
             血量--;
             血量文字.text = 血量.ToString();
-            float 血量比例 = (float)血量 / 100f;
-            print(血量比例);
+            float 血量比例 = (float)血量 / (float)原始血量;
             血條.localScale = new Vector3(血量比例, 1, 1);
+            if (血量 <= 0) 
+            { 
+                Destroy(this.gameObject); 
+            }
 
-            if(血量 <= 0)
+            if (血量 <= 0)
             {
-                Destroy(this.gameObject);
+                動畫器.SetTrigger("IsGead");
+                Destroy(this.gameObject, 3f);
+            }
+
+            else
+            {
+                動畫器.SetTrigger("IsHit");
             }
         }
     }
